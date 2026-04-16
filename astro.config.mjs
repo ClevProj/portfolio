e1cb4050e28f5a12da1ed3058e4d84e88c83fd30
Cauã Oliveira <deoliveiracauaugusto@gmail.com>
2026-04-16 11:30:44 -0300

    fix: import for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   devToolbar: {
